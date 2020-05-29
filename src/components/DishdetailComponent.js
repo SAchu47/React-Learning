7a7070e8e960c6b9bf47357b332bbe6fd8e80527
Coming___soON <sachinvilasnagane@karunya.edu.in>
2020-05-29 12:58:03 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,7 +19,7 @@ import { baseUrl } from '../shared/baseUrl';
         );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if(comments != null)
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -42,7 +42,7 @@ import { baseUrl } from '../shared/baseUrl';
                             })
                         }
                     </lu>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
         else {
@@ -87,7 +87,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <div className="row">
                         <RenderDish dish={props.dish} /> 
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
                     </div>
                 </div>
@@ -128,7 +128,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         console.log("Current State is" + JSON.stringify(values));
         alert("Current State is" + JSON.stringify(values));
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
